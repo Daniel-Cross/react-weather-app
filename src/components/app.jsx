545a68890c4949b2fa9d3075c6b8d0963cb38ac3
Daniel-Cross <crossy1686@gmail.com>
2018-08-01 13:11:19 +0100

    added props

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = props => (
+  <h1>
+    {`Hello ${props.name}`}
+  </h1>);
+
+App.propTypes = {
+  name: PropTypes.string,
+};
+
+App.defaultProps = {
+  name: 'World',
+};
+
+export default App;
